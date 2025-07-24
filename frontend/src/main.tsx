82dec40f739e40b306d4d4dffeba4edb5bfdbd4a
Nav1 <snithfferx@outlook.com>
2025-07-24 00:49:55 -0600

    COCOA | Updating | main, adding routes and changin render.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import ReactDOM from "react-dom/client";
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')!).render(
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
