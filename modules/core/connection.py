43f85f43af50d9e1f30a4060f51364758a42b1c4
Nav1 <snithfferx@outlook.com>
2025-08-07 22:57:15 -0600

    COCOA | Updating | Connection

--- modules/core/connection.py
+++ modules/core/connection.py
@@ -5,10 +5,19 @@ import os
 load_dotenv()
 
 def get_db_connection():
-    return mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
-    )
+    try:
+        return mysql.connector.connect(
+            host=os.getenv("MYSQL_DB_HOST"),
+            port=os.getenv("MYSQL_DB_PORT"),
+            user=os.getenv("MYSQL_DB_USER"),
+            password=os.getenv("MYSQL_DB_PASS"),
+            database=os.getenv("MYSQL_DB_NAME")
+        )
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        return err
+    # except Exception as err:
+    #     print(f"Error: {err}")
+    #     return err
+    # finally:
+    #     return None
