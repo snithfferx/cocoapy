3b6e6b416c28fcb52dea2a0d9ca0f68d046c1f82
Nav1 <snithfferx@outlook.com>
2025-08-14 19:23:51 -0600

    COCOA | Updating | Routes | counter

--- routes/counter.py
+++ routes/counter.py
@@ -1,42 +1,43 @@
-from flask import Blueprint, request, jsonify
-from modules.colonies.coloniesController import contar_colonias_por_cuadrante
+from flask import Blueprint
+from modules.colonies.coloniesController import countColoniesByQuarters
 from flask_jwt_extended import jwt_required
+from flask_cors import cross_origin
+from flask import current_app
+from middlewares.req_res import get_json, success, bad_request
 
-counterBp = Blueprint('counter', __name__)
+counterBp = Blueprint('counter', __name__, url_prefix="/counter")
 
-counterBp.route('/contar', methods=['POST'])
+def origin():
+    # Lee el origen permitido desde la config de la app
+    return current_app.config.get("ALLOWED_ORIGIN", "*")
+
+@counterBp.route('/get-colonies', methods=['POST'])
 @jwt_required()
-def contar():
+@cross_origin(origins=lambda: origin(), supports_credentials=True)
+def getColonies():
     try:
-        file = request.files.get("file")
-        sensibility = request.form.get("sensitivity", type=int, default=50)
-        quarters = request.form.get("quarters",type=int,default=2)
-        name = request.form.get("name",type=str,default='example_')
+        data = get_json()
+        file = data.get("file")
+        user = data.get("user")
+        description = data.get("description")
+        sensibility = data.get("sensitivity", type=int, default=50)
+        quarters = data.get("quarters",type=int,default=2)
+        name = data.get("name",type=str,default='example_')
         if not file:
-            return jsonify({
-                'status': 'error',
-                'message': "Archivo no recibido"}), 400
+            return bad_request("Archivo no recibido")
         if file:
             contenido = file.read()
-            avg, ovi, totals, cuts = contar_colonias_por_cuadrante(BytesIO(contenido), sensibilidad=sensibility,cuadrantes=(quarters,quarters))
-            return jsonify({
-                'status': 'ok',
-                'data': {
-                    'avg': avg,
-                    'ovi':ovi,
+            result = countColoniesByQuarters(user,name,description,BytesIO(contenido), sensibility=sensibility,cuadrantes=(quarters,quarters))
+            """ {media, img_base64, colonias_totales, colonias_imagenes} """
+            return success({
+                    'avg': result['media'],
+                    'ovi':result['img_base64'],
                     'totals':{
-                        'quarters': [f"Cuadrante {i+1}" for i in range(len(totals))],
-                        'values': totals,
-                        'images': cuts
+                        'quarters': [f"Cuadrante {i+1}" for i in range(len(result['colonias_totales']))],
+                        'values': result['colonias_totales'],
+                        'images': result['colonias_imagenes']
                     },
                     'name': name
-                }
-            })
-        return jsonify({
-            'status': 'error',
-            'message': "Archivo no recibido"}), 400
+                }, 200)
     except Exception as e:
-        return jsonify({
-            'status': 'error',
-            'message': str(e)
-        }), 400
+        return bad_request(str(e))
